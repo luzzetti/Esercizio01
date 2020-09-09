b24496e5427ef608feb6d74dbe17fa3e17e6e96d
luzzetti <christian.luzzetti@gmail.com>
2020-09-09 23:49:27 +0200

    TUtto

--- src/main/java/it/luzzetti/controller/AnimaliController.java
+++ src/main/java/it/luzzetti/controller/AnimaliController.java
@@ -1,28 +1,37 @@
 package it.luzzetti.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import it.luzzetti.entity.Animale;
+import it.luzzetti.service.AnimaliService;
 
 @Controller
 @RequestMapping("/animali")
 public class AnimaliController {
 
+	@Autowired
+	AnimaliService animaliService;
+	
 	@GetMapping("/")
-	public String getListaAnimali() {
-		//
-		return "lista-animali";
+	public String getListaAnimali(Model model) {
+		model.addAttribute("listaAnimali", animaliService.leggiAnimali());
+		return "animali/lista-animali";
 	}
 
 	@GetMapping("/new")
 	public String nuovoAnimaleDaAdottare() {
-		return null;
+		return "animali/nuovo-animale";
 	}
 	
 	@PostMapping("/")
-	public String creaAnimaleDaAdottare() {
-		return null;
+	public String creaAnimaleDaAdottare(Animale animale) {
+		animaliService.creaAnimale(animale);
+		return "redirect:/animali/";
 	}
 	
 	
--- src/main/java/it/luzzetti/entity/Utente.java
+++ src/main/java/it/luzzetti/entity/Utente.java
@@ -1,5 +1,6 @@
 package it.luzzetti.entity;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.Entity;
@@ -13,11 +14,48 @@ public class Utente {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	Long id;
-	String nome;
-	String cognome;
+	private Long id;
+	
+	private String nome;
+	
+	private String cognome;
 	
 	@OneToMany
-	List<Animale> animali;
+	List<Animale> animali = new ArrayList<>();
+
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCognome() {
+		return cognome;
+	}
+
+	public void setCognome(String cognome) {
+		this.cognome = cognome;
+	}
+
+	@Override
+	public String toString() {
+		return "Utente [id=" + id + ", nome=" + nome + ", cognome=" + cognome + ", animali=" + animali + "]";
+	}
+
+	public void adottaAnimale(Animale animaleTemporaneo) {
+		this.animali.add(animaleTemporaneo);
+	}
+	
 	
 }
