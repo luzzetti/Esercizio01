6114a96c371ec93133600c423c3ff79c48c82756
luzzetti <christian.luzzetti@gmail.com>
2020-09-09 21:41:45 +0200

    Commit Iniziale

---
+++ src/main/java/it/luzzetti/AdozioniAnimaliApplication.java
@@ -0,0 +1,13 @@
+package it.luzzetti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdozioniAnimaliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdozioniAnimaliApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/luzzetti/AdozioniAnimaliApplicationTests.java
@@ -0,0 +1,13 @@
+package it.luzzetti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdozioniAnimaliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
